2d679b0a6d0f014f85554cba1aff101db83b83b7
Divyansh Patel <nealbay@gmail.com>
2017-05-01 14:22:13 +0530

    Test setup and Timer/Coundown component

--- public/bundle.js
+++ public/bundle.js
@@ -26014,7 +26014,7 @@
 
 
 	// module
-	exports.push([module.id, ".top-bar, .top-bar ul {\n  background-color: #333333; }\n\n.top-bar .menu-text {\n  color: white;\n  padding: 0; }\n\n.menu > .menu-text > a {\n  display: inline;\n  padding: 0; }\n", ""]);
+	exports.push([module.id, ".top-bar, .top-bar ul {\n  background-color: #333333; }\n\n.top-bar .menu-text {\n  color: white;\n  padding: 0; }\n\n.menu > .menu-text > a {\n  display: inline;\n  padding: 0; }\n\n.top-bar .active-link {\n  font-weight: bold; }\n", ""]);
 
 	// exports
 
